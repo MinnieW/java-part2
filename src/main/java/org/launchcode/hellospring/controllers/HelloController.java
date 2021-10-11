f788eba2bc1d7649cbdd85699de3e55432e8613f
Minnie Washington <minniewashington@Minnies-MacBook-Pro.local>
2021-10-11 16:59:03 -0500

    complete exercise

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -18,33 +18,69 @@ public class HelloController {
 //    }
 // lives at /hello/goodbye
     @GetMapping("goodbye")
-    public String goodbye(){
+    public String goodbye() {
         return "Goodbye, Spring";
     }
 
     //Handles request of the form /hello?name=LaunchCode
 // lives at /hello
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name){
+    public String helloWithQueryParam(@RequestParam String name) {
         return "Hello, " + name + "!";
     }
 
     //Handles request of the form /hello/LaunchCode
 // lives at /hello/name
     @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name){
+    public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
-// lives at hello/form
+
+    // lives at hello/form
     @GetMapping("form")
-    public String helloForm(){
+    public String helloForm() {
         return "<html>" +
                 "<body>" +
                 "<form action='hello' method='post'>" + //submit a request to /hello
                 "<input type= 'text' name='name'>" +
+                "<select name='language'>" +
+                "<option value='1'>English</option>" +
+                "<option value='2'>Chinese</option>" +
+                "<option value='3'>Korean</option>" +
+                "<option value='4'>French</option>" +
+                "<option value='5'>Spanish</option>" +
+                "</select>" +
                 "<input type='submit' value='Great me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "hello")
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam int language){
+        return createMessage(name, language);
+    }
+
+    public static String createMessage(String name, int language){
+        String greeting = "";
+
+        if (language == 1){
+            greeting = "<h1>Helllo " + name +"</h1>";
+        }
+        if (language == 2){
+            greeting = "<h1>Ni Hao " + name +"</h1>";
+        }
+        if (language == 3){
+            greeting = "<h2>Ahn young haseyo " + name+"</h2>";
+        }
+        if (language == 4){
+            greeting = "<h2>Bonjour " + name+"</h2>";
+        }
+        if (language == 5){
+            greeting = "<h3>Hola " + name+"</h3>";
+        }
+
+        return greeting;
     }
 }
